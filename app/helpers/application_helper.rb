ff62537475e53c23fb438daca6b198ccd228bd12
Ian Jacobi <pipian@pipian.com>
2013-02-16 18:11:48 +0000

    Fix comments search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -112,6 +112,7 @@ module ApplicationHelper
   end
   
   def search_field(method, options = {})
+    method = method.to_s
     name = options[:label] || method.titleize
     raw '<div class="input"><label for="search_' + method + '">' + name + '</label><input type="text" name="search_' + method + '" id="search_'  + method + '"></div>'
   end
