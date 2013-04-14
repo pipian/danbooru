a62de7a18b065f9981299ab3357f670da1123f2a
Ian Jacobi <pipian@pipian.com>
2013-04-14 22:04:27 +0000

    Let the API work (side-effect: no hourly limiting)

--- app/logical/api_limiter.rb
+++ app/logical/api_limiter.rb
@@ -1,8 +1,9 @@
 module ApiLimiter
   def throttled?(ip_addr)
-    key = "#{ip_addr}:#{Time.now.hour}"
-    MEMCACHE.fetch(key, 1.hour, true) {0}
-    MEMCACHE.incr(key).to_i > CurrentUser.user.api_hourly_limit
+#    key = "#{ip_addr}:#{Time.now.hour}"
+#    MEMCACHE.fetch(key, 1.hour, true) {0}
+#    MEMCACHE.incr(key).to_i > CurrentUser.user.api_hourly_limit
+    false
   end
   
   module_function :throttled?
